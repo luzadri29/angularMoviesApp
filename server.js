a603a35e5ed89b2fd1883f195b7aef6dfb26842e
LUZ\LuzAdriana <luzadri@suyland.com>
2017-11-04 13:42:14 -0500

    route movies and person

--- server.js
+++ server.js
@@ -16,13 +16,23 @@ app.use(bodyParser.json());
 
 app.use(express.static(__dirname + '/dist'));
 
-router.get('/movies/:titleName', function(req, res){
-  console.log('recibing movie call '+ req.params.titleName);
+router.get('/movies/:findBy/:value', function(req, res){
+  var lookIn;
+  var findBy = req.params.findBy;
+  var value = req.params.value;
+  console.log('recibing movie call '+ value);
+
+  if(findBy == "movieName"){
+    lookIn = "movie";
+  }else if(findBy == "actorName"){
+    lookIn = "person";
+  }
+
   var options = {
   "method": "GET",
   "hostname": "api.themoviedb.org",
   "port": null,
-  "path": "/3/search/movie?query="+req.params.titleName+"&include_adult=false&page=1&language=en-US&api_key=3faf809df83252f672252023c1712984",
+  "path": "/3/search/"+lookIn+"?query="+value+"&include_adult=false&page=1&language=en-US&api_key=3faf809df83252f672252023c1712984",
   "headers": {}
 };
 var movieDBReq = http.request(options, function (movieDBRes) {
@@ -43,10 +53,11 @@ var movieDBReq = http.request(options, function (movieDBRes) {
 
 app.use('/api', router);
 
-app.get('/*', function(req, res) {
+/*app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
-
+*/
+app.use(express.static(__dirname + '/dist'));
 // start server
 app.listen(port, function() {
     console.log("App is running on port " + port);
