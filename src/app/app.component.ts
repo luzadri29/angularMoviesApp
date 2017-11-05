96cd1565658b147024cb52fe79dc7ef0fe0dad37
LUZ\LuzAdriana <luzadri@suyland.com>
2017-11-05 11:01:18 -0500

    delete appmovie/add in component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,6 @@
 import { Component } from '@angular/core';
-
-import { Movies } from './app.movies';
-
-
+import { Http }    from '@angular/http';
+import { MovieResponse }    from './app.movieresponse';
 
 @Component({
   selector: 'app-root',
@@ -12,21 +10,64 @@ import { Movies } from './app.movies';
 export class AppComponent {
 
   title : string;
-//  changeTitleP : string;
-
-  constructor(private movie: Movies ) {}
-
-
- changeTitle = this.movie.changeTitleP()
-      .then((val) => this.title = val);
+  clickMessage = '';
+  values = '';
+  findBy = 'movieName';
+  movieList = [];
+  result = 'from movies';
 
 
-getNowPlaying = this.movie.getNowPlaying()
+  constructor(private http: Http, ) { }
+
+
+  nowPlayingVariable =  this.getNowPlaying()
         .then((val) => {
           let data = val;
           console.log(data.page);
           console.log(data.results);
-          this.movie.movieList = data.results;
+          this.movieList = data.results;
         })
+
+
+        onKey(event: any) { // without type info
+          this.values = event.target.value;
+        }
+
+        onClickFind() {
+          this.getByQuery(this.findBy, this.values).then((val) => {
+            let data = val;
+            //console.log(data.results);
+            this.movieList = data.results;
+        })
+      }
+
+        onChange(event: any){
+            console.log("onChange escuchando "+ event.target.value);
+            this.findBy = event.target.value;
+        }
+
+
+        getNowPlaying(){
+           return  new Promise<MovieResponse>((resolve, reject) => {
+             this.http.get("/api/movies")
+             .toPromise()
+             .then( response => {
+               resolve(response.json().data)
+             })
+             .catch(err => console.log(err))
+           });
+        }
+
+        getByQuery(findBy, query){
+          return  new Promise<MovieResponse>((resolve, reject) => {
+            this.http.get("/api/movies/"+findBy+"/"+query)
+            .toPromise()
+            .then( response => {
+              resolve(response.json().data)
+            })
+            .catch(err => console.log(err))
+          });
+        }
+
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpModule }    from '@angular/http';
-import { Movies } from './app.movies';
 import { AppComponent } from './app.component';
-import { ClickFindComponent } from './click-find.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ClickFindComponent
+    AppComponent
+
   ],
   imports: [
     BrowserModule,
     HttpModule
   ],
-  providers: [Movies],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
